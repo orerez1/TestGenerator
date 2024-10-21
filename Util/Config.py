1669313d599f3a3d6f48ad5791ba4f2db0ead473
orerez1 <orerez09@gmail.com>
2024-10-21 11:13:10 +0300

    Reducing the number of tests

--- Util/Config.py
+++ Util/Config.py
@@ -1,4 +1,4 @@
-number_of_standard_tests_per_function = 3
-number_of_edge_case_tests_per_function_parameter = 2
+number_of_standard_tests_per_function = 2
+number_of_edge_case_tests_per_function_parameter = 1
 number_of_null_case_tests_per_function_parameter = 1
 number_of_expected_error_tests_per_thrown_exception = 1
