6e79d554cb60dbcf3e384a2527674f32b8f67a94
orerez1 <orerez09@gmail.com>
2024-11-02 18:50:47 +0200

    Formatting file

--- Util/Templates.py
+++ Util/Templates.py
@@ -1,4 +1,5 @@
 import platform
+
 # import sys
 # import os
 # idea_projects_path = os.getcwd()  # Example. fill with the path to your "IdeaProjects" directory
@@ -6,14 +7,21 @@ import platform
 test_class_path = 'C:\\Users\\orkin\\IdeaProjects\\Testing\\src\\Weird.java'
 
 os_name = platform.system().lower()
-separator = "\\" if os_name.__contains__("windows") else "/"   # Designed for Windows and Linux
+separator = (
+    "\\" if os_name.__contains__("windows") else "/"
+)  # Designed for Windows and Linux
 # path_param = sys.argv[1]
 tests_folder_name = "tests"
 path_param = test_class_path
-idea_projects_path = f"{path_param.split(f'IdeaProjects{separator}')[0]}IdeaProjects{separator}"
+idea_projects_path = (
+    f"{path_param.split(f'IdeaProjects{separator}')[0]}IdeaProjects{separator}"
+)
 
 path_to_project = f"{idea_projects_path}PROJECT_NAME{separator}"
-path_to_test_classes = path_to_project + tests_folder_name + separator + "CLASS_NAMETest.java"
+path_to_test_classes = (
+    path_to_project + tests_folder_name + separator + "CLASS_NAMETest.java"
+)
+
 
 def get_instance_call(class_name: str, is_singleton: bool) -> str:
     """
@@ -28,10 +36,17 @@ def get_instance_call(class_name: str, is_singleton: bool) -> str:
     """
     return f"{class_name}.getInstance" if is_singleton else f"new {class_name}"
 
+
 def create_standard_test(
-    function_in_name: str, test_number: str, return_type: str,
-    class_name: str, java_function: str, sending_params: str,
-    params: str, is_singleton: bool) -> str:
+    function_in_name: str,
+    test_number: str,
+    return_type: str,
+    class_name: str,
+    java_function: str,
+    sending_params: str,
+    params: str,
+    is_singleton: bool,
+) -> str:
     """
     Generates a standard test template for a given Java function.
 
@@ -68,9 +83,16 @@ def create_standard_test(
 
 
 def create_edge_case_test(
-    function_in_name: str, param_name: str, test_number: str,
-    return_type: str, class_name: str, java_function: str,
-    sending_params: str, params: str, is_singleton: bool) -> str:
+    function_in_name: str,
+    param_name: str,
+    test_number: str,
+    return_type: str,
+    class_name: str,
+    java_function: str,
+    sending_params: str,
+    params: str,
+    is_singleton: bool,
+) -> str:
     """
     Generates an edge case test template for a given Java function.
 
@@ -96,7 +118,7 @@ def create_edge_case_test(
     # returns an empty string if the function is "getInstance" or the same as the class name because we don't want to test constructors
     if java_function in ["getInstance", class_name]:
         return ""
-    
+
     return f"""
 \t@Test
 \tpublic void test{function_in_name}EdgeCase{param_name}{test_number}() {{
@@ -106,10 +128,19 @@ def create_edge_case_test(
 \t\tassertEquals(EXPECTED, RESULT);
 \t}}\n"""
 
+
 def create_null_edge_case_test(
-    function_in_name: str, param_name: str, test_number: str,
-    return_type: str, class_name: str, java_function: str,
-    sending_params: str, params: str, is_singleton: bool, param_type: str) -> str:
+    function_in_name: str,
+    param_name: str,
+    test_number: str,
+    return_type: str,
+    class_name: str,
+    java_function: str,
+    sending_params: str,
+    params: str,
+    is_singleton: bool,
+    param_type: str,
+) -> str:
     """
     Generates a null edge case test template for a given Java function.
 
@@ -135,18 +166,38 @@ def create_null_edge_case_test(
     """
 
     # primitive types cannot be null
-    primitive_types = ["boolean", "char", "byte", "short", "int", "long", "float", "double"]
+    primitive_types = [
+        "boolean",
+        "char",
+        "byte",
+        "short",
+        "int",
+        "long",
+        "float",
+        "double",
+    ]
 
     # returns an empty string if the function is "getInstance" or the same as the class name because we don't want to test constructors
     if java_function in ["getInstance", class_name] or param_type in primitive_types:
         return ""
-    
+
     # lower the param name to match the test method naming convention
-    lowered_param_name = param_name.lower() if len(param_name) <= 1 else param_name.lower()[0] + param_name.lower()[1:]
+    lowered_param_name = (
+        param_name.lower()
+        if len(param_name) <= 1
+        else param_name.lower()[0] + param_name.lower()[1:]
+    )
 
     # remove the test param from the test method parameters
-    params = ";\n".join([param for param in params.split(";\n") if param == '' or param.split(" = ")[-2].split(" ")[-1] != lowered_param_name])
-    
+    params = ";\n".join(
+        [
+            param
+            for param in params.split(";\n")
+            if param == ""
+            or param.split(" = ")[-2].split(" ")[-1] != lowered_param_name
+        ]
+    )
+
     # replace the param name with null in the test method parameters
     sending_params = sending_params.replace(lowered_param_name, "null")
     return f"""
@@ -158,6 +209,38 @@ def create_null_edge_case_test(
 \t\tassertEquals(EXPECTED, RESULT);
 \t}}\n"""
 
+
+def create_exception_test(
+    function_in_name: str,
+    exception: str,
+    class_name: str,
+    java_function: str,
+    sending_params: str,
+    test_number: str,
+    params: str,
+    is_singleton: bool,
+) -> str:
+    """
+    Generates an exception-throwing test template for a given Java function.
+
+    Args:
+        function_in_name (str): The name of the function, capitalized for test method naming.
+        exception (str): The exception expected to be thrown.
+        class_name (str): The name of the class containing the function.
+        java_function (str): The name of the function being tested.
+        sending_params (str): The parameters to be passed to the function during the test.
+        test_number (str): A unique identifier for the test.
+
+    Returns:
+        str: A formatted Java test method for exception throwing as a string.
+    """
+
+    return f"""
+\t@Test(expectedExceptions = {exception}.class)
+\tpublic void test{function_in_name}Throws{exception}{test_number}() {{
+{params}
+\t\t{get_instance_call(class_name, is_singleton)}().{java_function}({sending_params});
+\t}}"""
 
 
 standard_test = """\t@Test
@@ -185,7 +268,7 @@ null_case_test = """\t@Test
 \t}"""
 
 expected_error_test = """\t@Test(expectedExceptions = EXCEPTION.class)
-\tpublic void testFUNCTION_IN_NAMEThrowsEXCEPTION() {
+\tpublic void testFUNCTION_IN_NAMEThrowsEXCEPTIONTEST_NUMBER() {
 \t\tnew CLASS_NAME(?).FUNCTION(?);
 \t}"""
 
