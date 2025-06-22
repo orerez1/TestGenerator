daa15d15b9ec65e8ddbb210df5db385875ab09a5
orerez1 <85437583+orerez1@users.noreply.github.com>
2025-06-22 18:24:42 +0300

    Update TestCreator.py

--- Src/TestCreator.py
+++ Src/TestCreator.py
@@ -364,7 +364,7 @@ class TestCreator:
             test_class_file.close()
             if result.__contains__(Templates.eof):
                 tests = self.create_tests(existing_tests=result)
-                result = result.replace(Templates.eof, "\n\n" + tests)
+                result = result.replace(Templates.eof, "\n" + tests)
             else:
                 result = self.create_test_class_from_template()
         else:
